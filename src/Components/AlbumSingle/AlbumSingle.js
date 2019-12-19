a93822fd351e8e2c5bf2797723e087ed86c40cce
Atahualpa Pena <atahualpapena04@gmail.com>
2019-12-19 13:23:53 -0700

    refactored albumsingle

--- src/Components/AlbumSingle/AlbumSingle.js
+++ src/Components/AlbumSingle/AlbumSingle.js
@@ -1,66 +1,26 @@
 import React from "react";
+import { Typography } from "@material-ui/core";
+import useReactRouter from "use-react-router";
 import axios from "axios";
-import { Link } from "react-router-dom";
-import { Container, Card } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
 
-const useStyles = makeStyles({
-  card: {
-    maxWidth: 275,
-    minHeight: 275,
-    margin: 20,
-
-    display: "inline-block"
-  },
-  title: {
-    fontSize: 14
-  },
-  pos: {
-    marginBottom: 12
-  }
-});
-
-const AlbumSingle = ({ match }) => {
-  const classes = useStyles();
-  const [album, setAlbum] = React.useState([]);
+const AlbumSingle = () => {
+  const { match } = useReactRouter();
+  const { id } = match.params;
+  const [album, setAlbum] = React.useState();
   React.useEffect(() => {
-    axios
-      .get(`https://jsonplaceholder.typicode.com/albums/${album.id}`)
-      .then(res => {
-        setAlbum(res.data);
-      });
-  }, []);
+    const fetchAlbums = async () => {
+      const res = await axios.get(`http://localhost:3000/albums/${id}`);
+      setAlbum(res.data);
+      console.log(res);
+    };
+    fetchAlbums();
+  }, [match.params.id]);
 
   return (
-    <Container className="Album-card">
-      <br />
-      <Typography variant="h5" component="h3">
-        Select an Album
-      </Typography>
-      <Card className={classes.card}>
-        <CardContent>
-          <Typography
-            className={classes.title}
-            color="textSecondary"
-            gutterBottom
-          >
-            Abum Id: {album.id}
-          </Typography>
-          <Typography variant="h5" component="h2">
-            Album title: {album.title}
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Link to="/photos">
-            <Button size="small">Go to Photos</Button>
-          </Link>
-        </CardActions>
-      </Card>
-    </Container>
+    <div>
+      {!album && <Typography variant="h5">Loading</Typography>}
+      {album && <Typography variant="h6">{album.title}</Typography>}
+    </div>
   );
 };
 
