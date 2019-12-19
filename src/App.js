533a25e0d5d8474a14e69f5d5ad7ad63372ce4af
Atahualpa Pena <atahualpapena04@gmail.com>
2019-12-19 16:00:56 -0700

    fixed route

--- src/App.js
+++ src/App.js
@@ -14,9 +14,9 @@ function App() {
         <NavBar />
         <Switch>
           <Route path="/" exact component={Home} />
+          <Route path="/photos/:id" exact component={PhotoItem} />
           <Route path="/albums" exact component={AlbumList} />
-          <Route path="/album/:id" exact component={AlbumSingle} />
-          <Route path="/album/:id/photos" exact component={PhotoItem} />
+          <Route path="/albums/:id" exact component={AlbumSingle} />
         </Switch>
       </div>
     </Router>
