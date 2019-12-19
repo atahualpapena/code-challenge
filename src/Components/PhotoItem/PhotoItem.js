14e60c871f123b3afd6f38b617c23c4bb75d3fe4
Atahualpa Pena <atahualpapena04@gmail.com>
2019-12-19 16:04:45 -0700

    refactored photo item component

--- src/Components/PhotoItem/PhotoItem.js
+++ src/Components/PhotoItem/PhotoItem.js
@@ -1,66 +1,51 @@
 import React from "react";
 import axios from "axios";
+import useReactRouter from "use-react-router";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardHeader from "@material-ui/core/CardHeader";
 import CardMedia from "@material-ui/core/CardMedia";
 import CardActions from "@material-ui/core/CardActions";
 import IconButton from "@material-ui/core/IconButton";
-import { red } from "@material-ui/core/colors";
+
 import ArrowBackIcon from "@material-ui/icons/ArrowBack";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles(() => ({
   card: {
-    maxWidth: 375,
-    margin: 20,
-    display: "inline-block"
+    maxWidth: 345,
+    marginTop: 20,
+    margin: "auto"
   },
   media: {
     height: 0,
-    paddingTop: "56.25%" // 16:9
-  },
-  expand: {
-    transform: "rotate(0deg)",
-    marginLeft: "auto",
-    transition: theme.transitions.create("transform", {
-      duration: theme.transitions.duration.shortest
-    })
-  },
-  expandOpen: {
-    transform: "rotate(180deg)"
-  },
-  avatar: {
-    backgroundColor: red[500]
+    paddingTop: "56.25%"
   }
 }));
 
 const PhotoItem = () => {
   const classes = useStyles();
-  const [photos, setPhotos] = React.useState([]);
+
+  const { match } = useReactRouter();
+  const { id } = match.params;
+  const [photo, setPhotos] = React.useState([]);
   React.useEffect(() => {
-    axios.get("http://localhost:3000/photos").then(res => {
+    const fetchPhotos = async () => {
+      const res = await axios.get(`http://localhost:3000/photos/${id}`);
+      console.log(res.data);
       setPhotos(res.data);
-    });
+    };
+    fetchPhotos();
   }, []);
   return (
-    <div>
-      <h1>Photos</h1>
-      {photos.map(photo => (
-        <Card className={classes.card}>
-          <CardHeader title={photo.title} />
-          <CardMedia
-            className={classes.media}
-            image={photo.thumbnailUrl}
-            title={photo.title}
-          />
-          <CardActions disableSpacing>
-            <IconButton aria-label="back">
-              <ArrowBackIcon />
-            </IconButton>
-          </CardActions>
-        </Card>
-      ))}
-    </div>
+    <Card className={classes.card}>
+      <CardHeader title={photo.title} subheader={id} />
+      <CardMedia className={classes.media} image={photo.thumbnailUrl} />
+      <CardActions disableSpacing>
+        <IconButton aria-label="Go To Albums">
+          <ArrowBackIcon />
+        </IconButton>
+      </CardActions>
+    </Card>
   );
 };
 
