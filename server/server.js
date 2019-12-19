e632cbe21c55d460e09a73b91a5761651d6a581b
Atahualpa Pena <atahualpapena04@gmail.com>
2019-12-19 13:23:17 -0700

    working on issue with :id route

--- server/server.js
+++ server/server.js
@@ -25,12 +25,31 @@ app.get("/photos", (req, res) => {
 
 app.get("/albums", (req, res) => {
   axios
-    .get("https://jsonplaceholder.typicode.com/albums?_limit=10")
+    .get("https://jsonplaceholder.typicode.com/albums")
     .then(function(response) {
       res.json(response.data);
     })
     .catch(function(error) {
-      res.json("Error occured!");
+      res.json(`Error occured: ${error}`);
+    });
+});
+
+app.get("/albums/:id", (req, res, next) => {
+  console.log(req.params.id);
+  res.send({
+    title: "hello",
+    id: 1,
+    extra: "hhahaah"
+  });
+  next();
+  axios
+    .get(`https://jsonplaeholder.typicode.com/albums/${req.params.id}`)
+    .then(function(response) {
+      console.log(response.data);
+      res.json(response.data);
+    })
+    .catch(function(error) {
+      res.json(`Error ocurred: ${error}`);
     });
 });
 
